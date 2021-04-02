1239be3d412eae394d2a246b4270a61b148f9be4
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-04-02 22:57:26 +0300

    Add get all tasks to service and controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = ['task1'];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
