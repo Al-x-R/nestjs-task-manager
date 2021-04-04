8cf611a6ca090536d44e529fe998918fae3c7ba7
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-04-04 23:37:50 +0300

    Add error handling for a non-existing task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,14 @@
 import {
   Controller,
+  UsePipes,
   Param,
   Query,
   Body,
   Delete,
   Patch,
   Post,
-  Get, UsePipes, ValidationPipe,
+  Get,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
