7456dc887e9ec34e45cde402f0b3e01160de33f0
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-04-05 17:48:46 +0300

    Add hashing for user password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import {
@@ -8,12 +9,17 @@ import {
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
